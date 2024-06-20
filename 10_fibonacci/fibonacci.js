246380e6726bfe416ecbe9e097ea668e5664d1ae
OpkWan <alexopoku83@gmail.com>
2024-06-20 16:59:29 +0000

    Exercise Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const num = parseInt(n, 10);
+    if (num < 0) {
+      return "OOPS";
+    } else if (isNaN(num)) {
+      throw new Error("Input must be a number");
+    } else if (num <= 1) {
+      return num;
+    } else {
+      return fibonacci(num-1) + fibonacci(num-2);
+    }
+    
 };
 
 // Do not edit below this line
